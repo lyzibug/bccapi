afa48d140e1d422ffdd9ddc3ca580dd20f37b34a
jan.moller <jan.moller@gmail.com>
2011-11-25 11:34:12 +0000

    Fixed a bug in Base58.decode() where we had an index out of bounds if the encoded string contained only '1' characters

--- src/com/bccapi/core/Base58.java
+++ src/com/bccapi/core/Base58.java
@@ -64,7 +64,7 @@ public class Base58 {
       boolean stripSignByte = bytes.length > 1 && bytes[0] == 0 && bytes[1] < 0;
       // Count the leading zeros, if any.
       int leadingZeros = 0;
-      for (int i = 0; input.charAt(i) == ALPHABET.charAt(0); i++) {
+      for (int i = 0; i < input.length() && input.charAt(i) == ALPHABET.charAt(0); i++) {
          leadingZeros++;
       }
       // Now cut/pad correctly. Java 6 has a convenience for this, but Android
